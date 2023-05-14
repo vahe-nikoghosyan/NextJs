6487554e0bb4de85c0999ea5da5f2d7293a707b7
vahenikoghosyan <vahenikoghosyan@doublecoconut.com>
2023-05-14 22:25:50 +0400

    feat: Extracting Dynamic Path Segment data (Dynamic routes)

--- pages/portfolio/[id].tsx
+++ pages/portfolio/[id].tsx
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 function PortfolioProject() {
+  const router = useRouter();
+
+  console.log(router.pathname); // portfolio/[id]
+  console.log(router.query); // { id: "1" }
+
+  // send request to some backend server
   return (
     <div>
       <h1>The portfolio project page</h1>
