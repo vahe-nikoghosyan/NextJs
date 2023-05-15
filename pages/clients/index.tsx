18a9c6cfcde877e0da66a5028ca05371c5c12d5b
vahe-dc <vahenikoghosyan@doublecoconut.com>
2023-05-15 13:28:55 +0400

    feat: navigate to dynamic routes

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 function Clients() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manual" },
+  ];
   return (
     <div>
       <h1>Clients page</h1>
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
