e6d2161a9f5eb1847fd53f83edc7b86ba98355e5
vahe-dc <vahenikoghosyan@doublecoconut.com>
2023-05-15 14:03:52 +0400

    feat: navigating programmatically

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -4,9 +4,20 @@ function ClientProject() {
   const router = useRouter();
   console.log(router.query); // {id: '1'}
 
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/max/project1");
+    // router.replace("/clients/max/project1");
+    router.push({
+      pathname: "/clients/[id]/[clientProjectId]",
+      query: { id: "max", clientProjectId: "project1" },
+    });
+  }
+
   return (
     <div>
       <h1>Client Project page</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   );
 }
