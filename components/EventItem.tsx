d5f9a49f9e28f4c7e2a5d47506c97ce4f5581b72
vahe-dc <vahenikoghosyan@doublecoconut.com>
2023-05-18 18:21:50 +0400

    feat: styling components in Next.js projects

--- components/EventItem.tsx
+++ components/EventItem.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import classes from "./event-item.module.css";
+
 function EventItem(props: any) {
   const { title, image, date, location, id } = props;
 
@@ -13,20 +15,20 @@ function EventItem(props: any) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
+      <div className={classes.content}>
         <div>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
 
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore event</Link>
         </div>
       </div>
--- components/EventList.tsx
+++ components/EventList.tsx
@@ -1,24 +1,23 @@
 import EventItem from "./EventItem";
 
+import classes from "./event-list.module.css";
 function EventList(props: { items: any[] }) {
   const { items } = props;
   return (
-    <div>
-      <ul>
-        {items.map((event) => {
-          return (
-            <EventItem
-              key={event.id}
-              id={event.id}
-              title={event.title}
-              image={event.image}
-              location={event.location}
-              date={event.date}
-            />
-          );
-        })}
-      </ul>
-    </div>
+    <ul className={classes.list}>
+      {items.map((event) => {
+        return (
+          <EventItem
+            key={event.id}
+            id={event.id}
+            title={event.title}
+            image={event.image}
+            location={event.location}
+            date={event.date}
+          />
+        );
+      })}
+    </ul>
   );
 }
 
