cd03d7829eac44c59a35309ddaf9a2fc76573939
vahe-dc <vahenikoghosyan@doublecoconut.com>
2023-06-06 18:05:12 +0400

    work with "All events" page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
-import { getAllEvents } from "../../data/dummy-data";
+import { getAllEvents } from "../../heplers/api-utils";
 
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 import { useRouter } from "next/router";
 
-function Events() {
+function Events(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -20,6 +20,17 @@ function Events() {
       <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const allEvents = await getAllEvents();
+
+  return {
+    props: {
+      events: allEvents,
+    },
+    revalidate: 60,
+  };
 }
 
 export default Events;
